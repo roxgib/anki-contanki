35f6ba66a93979887df413ecc8ceb93c54c4f174
roxgib <33942237+roxgib@users.noreply.github.com>
2022-04-09 14:55:15 +1000

    Fix tooltip issue when using multiple controllers

--- contanki/controller.js
+++ contanki/controller.js
@@ -39,7 +39,7 @@ function on_controller_connect(event) {
         }
         bridgeCommand(register);
         connect_controller(i);
-        bridgeCommand("contanki::message::Multiple controllers detected. Pick which one you'd like to use from the Tools menu.");
+        bridgeCommand("contanki::message::Multiple controllers detected - pick which one you'd like to use from the Tools menu.");
     }
 }
 
