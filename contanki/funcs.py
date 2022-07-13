d4a0bf4422d907f6ee9efa130e6fc84a1a21c573
roxgib <33942237+roxgib@users.noreply.github.com>
2022-07-13 16:08:24 +1000

    Formatting

--- contanki/funcs.py
+++ contanki/funcs.py
@@ -6,7 +6,17 @@ from os.path import dirname, abspath, join, exists
 
 from aqt import mw
 from aqt.deckoptions import display_options_for_deck_id
-from aqt.qt import QCoreApplication, QKeySequence, QMouseEvent, QEvent, QPixmap, QPoint, QPointF, Qt, QPainter
+from aqt.qt import (
+    QCoreApplication,
+    QKeySequence,
+    QMouseEvent,
+    QEvent,
+    QPixmap,
+    QPoint,
+    QPointF,
+    Qt,
+    QPainter,
+)
 from aqt.qt import QKeyEvent as QKE
 from aqt.utils import current_window, tooltip
 from anki.decks import DeckId
@@ -16,29 +26,32 @@ addon_path = dirname(abspath(__file__))
 
 # Internal
 
+
 def get_state() -> str:
     if focus := current_window():
-        if focus.objectName() == 'MainWindow':
+        if focus.objectName() == "MainWindow":
             return mw.reviewer.state if mw.state == "review" else mw.state
-        elif focus.objectName() == 'Preferences':
-            return 'dialog'
-        elif focus.objectName() == 'Contanki Options':
-            return 'config'
+        elif focus.objectName() == "Preferences":
+            return "dialog"
+        elif focus.objectName() == "Contanki Options":
+            return "config"
         else:
-            return 'NoFocus'
+            return "NoFocus"
     else:
-        return 'NoFocus'
+        return "NoFocus"
 
 
 def for_states(states: List[str]) -> Callable:
     def decorater(func: Callable) -> Callable:
         def wrapped(*args, **kwargs) -> Any:
-            if get_state() in states: 
+            if get_state() in states:
                 return func(*args, **kwargs)
             else:
                 tooltip("Action not available on this screen")
+
         return wrapped
-    return decorater    
+
+    return decorater
 
 
 def _pass() -> None:
@@ -46,7 +59,7 @@ def _pass() -> None:
 
 
 def quad_curve(value: float, factor: int = 5) -> float:
-    return ((value * factor)**2) * value
+    return ((value * factor) ** 2) * value
 
 
 def _get_dark_mode() -> Callable:
@@ -57,16 +70,20 @@ def _get_dark_mode() -> Callable:
 
     if is_win:
         from aqt.theme import get_windows_dark_mode
+
         return get_windows_dark_mode
     elif is_mac:
-        from aqt.theme import get_macos_dark_mode    
+        from aqt.theme import get_macos_dark_mode
+
         return get_macos_dark_mode
     else:
         try:
             from aqt.theme import get_linux_dark_mode
+
             return get_linux_dark_mode
         except:
             return lambda: False
+
 
 get_dark_mode = _get_dark_mode()
 
@@ -74,31 +91,46 @@ get_dark_mode = _get_dark_mode()
 def get_button_icon(controller: str, button: str, glow: bool = False) -> QPixmap:
     if "(" in controller:
         controller = controller.split(" (")[0]
-    directions = ['Left','Right','Up','Down','Horizontal','Vertical', "Diagonal", "Cross"]
+    directions = [
+        "Left",
+        "Right",
+        "Up",
+        "Down",
+        "Horizontal",
+        "Vertical",
+        "Diagonal",
+        "Cross",
+    ]
+
     def path(button):
-        return join(addon_path, 'buttons', controller, button)
-    if exists(path(button) + '.png'):
+        return join(addon_path, "buttons", controller, button)
+
+    if exists(path(button) + ".png"):
         pixmap = QPixmap(path(button))
-    elif button.split(' ')[-1] in directions:
-        direction = button.split(' ')[-1]
-        button = ' '.join(button.split(' ')[:-1])
-        if not exists(path(button) + '.png'):
-            raise FileNotFoundError(f"Button {button} for controller {controller} not found")
+    elif button.split(" ")[-1] in directions:
+        direction = button.split(" ")[-1]
+        button = " ".join(button.split(" ")[:-1])
+        if not exists(path(button) + ".png"):
+            raise FileNotFoundError(
+                f"Button {button} for controller {controller} not found"
+            )
         pixmap = QPixmap(path(button))
-        dpixmap = QPixmap(join(addon_path, 'buttons', 'Arrows', direction))
+        dpixmap = QPixmap(join(addon_path, "buttons", "Arrows", direction))
         painter = QPainter()
         with QPainter(pixmap) as painter:
             painter.drawPixmap(pixmap.rect(), dpixmap, dpixmap.rect())
     else:
         pixmap = QPixmap(path(button))
-        
+
     if glow:
-        gpixmap = QPixmap(join(addon_path, 'buttons', 'Other', 'glow'))
+        gpixmap = QPixmap(join(addon_path, "buttons", "Other", "glow"))
         try:
             with QPainter(pixmap) as painter:
                 painter.drawPixmap(pixmap.rect(), gpixmap, gpixmap.rect())
         except ValueError as err:
-            raise ValueError(f"Button {button} for controller {controller} not found") from err
+            raise FileNotFoundError(
+                f"Button {button} for controller {controller} not found"
+            ) from err
     return pixmap
 
 
@@ -113,28 +145,32 @@ def get_custom_actions() -> Dict[str, Callable]:
         except:
             key = keys[0]
             modifier = Qt.KeyboardModifier.NoModifier
-        
+
         func = partial(keyPress, key, modifier)
         actions[action] = func
 
     return actions
 
 
-def get_file(file: str) -> str: # refactor this
+def get_file(file: str) -> str:  # refactor this
     paths = [
         addon_path,
-        join(addon_path, 'user_files'), 
-        join(addon_path, 'profiles'), 
-        join(addon_path, 'user_files', 'profiles'), 
+        join(addon_path, "user_files"),
+        join(addon_path, "profiles"),
+        join(addon_path, "user_files", "profiles"),
     ]
     for path in paths:
         if exists(join(path, file)):
             with open(join(path, file)) as f:
                 return f.read()
 
+
 # Common
 
-def keyPress(key: Qt.Key, mod: Qt.KeyboardModifier = Qt.KeyboardModifier.NoModifier) -> None:
+
+def keyPress(
+    key: Qt.Key, mod: Qt.KeyboardModifier = Qt.KeyboardModifier.NoModifier
+) -> None:
     QCoreApplication.sendEvent(mw.app.focusObject(), QKE(QKE.Type.KeyPress, key, mod))
     QCoreApplication.sendEvent(mw.app.focusObject(), QKE(QKE.Type.KeyRelease, key, mod))
 
@@ -152,49 +188,48 @@ def tab(value: float = 1) -> None:
 
 def scroll_build() -> Callable:
     config = mw.addonManager.getConfig(__name__)
-    speed = config['Scroll Speed'] / 10
-    deadzone = config['Stick Deadzone'] / 100
-    
-    def scroll (x: float, y: float) -> None:
-        if abs(x) + abs(y) < deadzone: return
-        mw.web.eval(f'window.scrollBy({quad_curve(x*speed)}, {quad_curve(y*speed)})')
+    speed = config["Scroll Speed"] / 10
+    deadzone = config["Stick Deadzone"] / 100
+
+    def scroll(x: float, y: float) -> None:
+        if abs(x) + abs(y) < deadzone:
+            return
+        mw.web.eval(f"window.scrollBy({quad_curve(x*speed)}, {quad_curve(y*speed)})")
 
     return scroll
 
+
 scroll = scroll_build()
+
 
 def move_mouse_build() -> Callable:
     config = mw.addonManager.getConfig(__name__)
-    speed = config['Cursor Speed'] / 2
-    accel = config['Cursor Acceleration'] / 5
-    deadzone = config['Stick Deadzone'] / 100
-    
+    speed = config["Cursor Speed"] / 2
+    accel = config["Cursor Acceleration"] / 5
+    deadzone = config["Stick Deadzone"] / 100
+
     def move_mouse(x: float, y: float) -> None:
-        if abs(x) + abs(y) < deadzone: return
+        if abs(x) + abs(y) < deadzone:
+            return
         cursor = mw.cursor()
         pos = cursor.pos()
         geom = mw.screen().geometry()
 
-        y = pos.y() + ((abs(y)*speed)**(accel+1))*y
-        x = pos.x() + ((abs(x)*speed)**(accel+1))*x
+        y = pos.y() + ((abs(y) * speed) ** (accel + 1)) * y
+        x = pos.x() + ((abs(x) * speed) ** (accel + 1)) * x
         x, y = max(x, geom.x()), max(y, geom.y())
         x, y = min(x, geom.width()), min(y, geom.height())
-        
+
         pos.setX(x)
         pos.setY(y)
         cursor.setPos(pos)
-    
+
     return move_mouse
 
 
 def hide_cursor() -> None:
     size = mw.screen().geometry()
-    mw.cursor().setPos(
-        QPoint(
-            size.width(), 
-            size.height()
-        )
-    )
+    mw.cursor().setPos(QPoint(size.width(), size.height()))
 
 
 def click(
@@ -203,11 +238,14 @@ def click(
 ) -> None:
     pos = mw.cursor().pos()
     widget = mw.app.widgetAt(pos)
-    if not widget: return
-    
-    widgetPostition = widget.mapToGlobal(QPoint(0,0))
+    if not widget:
+        return
+
+    widgetPostition = widget.mapToGlobal(QPoint(0, 0))
     localPos = QPointF(pos.x() - widgetPostition.x(), pos.y() - widgetPostition.y())
-    QCoreApplication.postEvent(widget, QMouseEvent(QEvent.Type.MouseButtonPress, localPos, button, button, mod))
+    QCoreApplication.postEvent(
+        widget, QMouseEvent(QEvent.Type.MouseButtonPress, localPos, button, button, mod)
+    )
 
     mw.web.eval('document.querySelectorAll( ":hover" )[0].click()')
 
@@ -218,13 +256,21 @@ def click_release(
 ) -> None:
     pos = mw.cursor().pos()
     widget = mw.app.widgetAt(pos)
-    if not widget: return
-    widgetPostition = widget.mapToGlobal(QPoint(0,0))
+    if not widget:
+        return
+    widgetPostition = widget.mapToGlobal(QPoint(0, 0))
     localPos = QPointF(pos.x() - widgetPostition.x(), pos.y() - widgetPostition.y())
 
     QCoreApplication.postEvent(
-        widget,QMouseEvent(QEvent.Type.MouseButtonRelease, localPos, button, Qt.MouseButton.NoButton, mod)
-        )
+        widget,
+        QMouseEvent(
+            QEvent.Type.MouseButtonRelease,
+            localPos,
+            button,
+            Qt.MouseButton.NoButton,
+            mod,
+        ),
+    )
 
 
 def on_enter() -> None:
@@ -234,9 +280,9 @@ def on_enter() -> None:
         mw.reviewer.onEnterKey()
     else:
         keyPress(Qt.Key.Key_Enter)
-        
 
-@for_states(['deckBrowser','review','overview','question','answer'])
+
+@for_states(["deckBrowser", "review", "overview", "question", "answer"])
 def back() -> None:
     if mw.state == "review":
         mw.moveToState("overview")
@@ -244,7 +290,7 @@ def back() -> None:
         mw.moveToState("deckBrowser")
 
 
-@for_states(['deckBrowser','review','overview','question','answer'])
+@for_states(["deckBrowser", "review", "overview", "question", "answer"])
 def forward() -> None:
     if mw.state == "deckBrowser":
         mw.moveToState("overview")
@@ -252,22 +298,25 @@ def forward() -> None:
         mw.moveToState("review")
 
 
-@for_states(['deckBrowser','review','overview','question','answer'])
+@for_states(["deckBrowser", "review", "overview", "question", "answer"])
 def on_options() -> None:
     def deck_options(did: str) -> None:
-        try: 
+        try:
             display_options_for_deck_id(DeckId(int(did)))
         except:
             mw.onPrefs()
+
     if mw.state == "review":
         mw.reviewer.onOptions()
     elif mw.state == "deckBrowser":
-        mw.web.evalWithCallback('document.activeElement.parentElement.parentElement.id', deck_options)
+        mw.web.evalWithCallback(
+            "document.activeElement.parentElement.parentElement.id", deck_options
+        )
     elif mw.state == "overview":
         display_options_for_deck_id(mw.col.decks.get_current_id())
 
 
-@for_states(['deckBrowser','review','overview','question','answer'])
+@for_states(["deckBrowser", "review", "overview", "question", "answer"])
 def toggle_fullscreen() -> None:
     if cw := current_window().window():
         if cw.isFullScreen():
@@ -295,17 +344,17 @@ def change_volume(direction=True):
         from aqt.utils import is_mac
     except:
         return
-        
+
     if is_mac:
         current_volume = re.search(
             r"volume:(\d\d)",
             subprocess.run(
-                'osascript -e "get volume settings"', 
+                'osascript -e "get volume settings"',
                 shell=True,
                 capture_output=True,
                 text=True,
-            ).stdout
-            )
+            ).stdout,
+        )
         if current_volume:
             current_volume = int(current_volume.group(1)) / 14
             current_volume += -0.5 + int(direction)
@@ -314,10 +363,11 @@ def change_volume(direction=True):
 
 ### Review
 
+
 def _cycle_flag() -> Callable:
     flags = mw.addonManager.getConfig(__name__)["Flags"]
 
-    @for_states(['question','answer'])
+    @for_states(["question", "answer"])
     def cycle_flag(flags):
         flag = mw.reviewer.card.flags
         if flag == 0:
@@ -331,28 +381,34 @@ def _cycle_flag() -> Callable:
 
     return lambda: cycle_flag(flags)
 
+
 cycle_flag = _cycle_flag()
 
 
-@for_states(['question','answer'])
+@for_states(["question", "answer"])
 def card_info():
     mw.reviewer.on_card_info()
+
 
 def _previous_card_info():
     try:
         f = mw.reviewer.on_previous_card_info
     except:
-        return _pass    
-    return for_states(['question','answer'])(f)
+        return _pass
+    return for_states(["question", "answer"])(f)
+
 
 previous_card_info = _previous_card_info()
 
 
 ### Deck Browser
 
+
 def _build_deck_list() -> List[Tuple[int, bool]]:
-    def _build_node(node):        
-        decks = [(node.deck_id, node.review_count or node.learn_count or node.new_count)]
+    def _build_node(node):
+        decks = [
+            (node.deck_id, node.review_count or node.learn_count or node.new_count)
+        ]
         if node.children:
             if not node.collapsed:
                 for child in node.children:
@@ -367,7 +423,9 @@ def _build_deck_list() -> List[Tuple[int, bool]]:
 
 
 def _select_deck(did) -> None:
-    mw.web.eval(f"document.getElementById({did}).getElementsByClassName('deck')[0].focus()")
+    mw.web.eval(
+        f"document.getElementById({did}).getElementsByClassName('deck')[0].focus()"
+    )
 
 
 def _choose_deck(c_deck: int, direction: bool, due: bool) -> None:
@@ -385,39 +443,39 @@ def _choose_deck(c_deck: int, direction: bool, due: bool) -> None:
     else:
         c_deck_index = -direction
 
-    c_deck_index += (1 if direction else -1)
+    c_deck_index += 1 if direction else -1
     while due and not dues[c_deck_index]:
-        c_deck_index += (1 if direction else -1)
+        c_deck_index += 1 if direction else -1
         if c_deck_index == len_decks:
             c_deck_index = -1
         if decks[c_deck_index] == 1:
-            c_deck_index += (1 if direction else -1)
-    
+            c_deck_index += 1 if direction else -1
+
     if len_decks == 1:
         c_deck_index = 0
 
-    if mw.state == 'deckBrowser':
+    if mw.state == "deckBrowser":
         _select_deck(decks[c_deck_index])
     else:
         mw.col.decks.select(decks[c_deck_index])
         mw.moveToState("overview")
 
 
-@for_states(['deckBrowser', 'overview'])
+@for_states(["deckBrowser", "overview"])
 def choose_deck(direction: bool, due: bool = False) -> None:
     mw.web.setFocus()
 
-    if mw.state == 'deckBrowser':
+    if mw.state == "deckBrowser":
         mw.web.evalWithCallback(
-            'document.activeElement.parentElement.parentElement.id', 
-            lambda c_deck: _choose_deck(c_deck, direction, due)
-            )
+            "document.activeElement.parentElement.parentElement.id",
+            lambda c_deck: _choose_deck(c_deck, direction, due),
+        )
     else:
         _choose_deck(mw.col.decks.get_current_id(), direction, due)
 
 
-@for_states(['deckBrowser'])
+@for_states(["deckBrowser"])
 def collapse_deck() -> None:
     mw.web.eval(
         "document.activeElement.parentElement.getElementsByClassName('collapse')[0].click()"
-        )
+    )
