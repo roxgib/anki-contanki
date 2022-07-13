2126137f5c0fb4dc91f54a8e8dafe25482984204
roxgib <33942237+roxgib@users.noreply.github.com>
2022-07-13 13:10:32 +1000

    Add controller to button image not found error

--- contanki/funcs.py
+++ contanki/funcs.py
@@ -81,7 +81,7 @@ def get_button_icon(controller: str, button: str, glow: bool = False) -> QPixmap
         direction = button.split(' ')[-1]
         button = ' '.join(button.split(' ')[:-1])
         if not exists(path(button) + '.png'):
-            raise FileNotFoundError(f"Button {button} not found")
+            raise FileNotFoundError(f"Button {button} for controller {controller} not found")
         pixmap = QPixmap(path(button))
         dpixmap = QPixmap(join(addon_path, 'buttons', 'Arrows', direction))
         painter = QPainter()
