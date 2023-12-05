2d0cad74f4e77b27627193b717f3729b75af3e08
Sam Bradshaw <samjr.bradshaw@gmail.com>
2023-12-05 11:34:38 +1100

    Add mock controller

--- contanki/controller.js
+++ contanki/controller.js
@@ -1,96 +1,127 @@
-let polling, connected_index, indices, ready;
-initialise()
+let polling, connected_index, indices, ready, mock_index;
+initialise();
 
 function initialise() {
-    if (ready) { return }
-    try {
-        bridgeCommand('contanki::initialise::arg');
-        // Causes a lot of console spam if we try to call bridgeCommand too early
-    } catch (err) {
-        setTimeout(initialise, 1000);
-        return;
-    }
-    window.addEventListener("gamepadconnected", on_controller_connect);
-    window.addEventListener("gamepaddisconnected", on_controller_disconnect);
-    ready = true;
+   if (ready) {
+      return;
+   }
+   try {
+      bridgeCommand("contanki::initialise::arg");
+      // Causes a lot of console spam if we try to call bridgeCommand too early
+   } catch (err) {
+      setTimeout(initialise, 1000);
+      return;
+   }
+   window.addEventListener("gamepadconnected", on_controller_connect);
+   window.addEventListener("gamepaddisconnected", on_controller_disconnect);
+   ready = true;
+   mock = false;
 }
 
 function on_controller_connect() {
-    let controllers = window.navigator.getGamepads();
-    let register = 'contanki::register';
-    indices = [];
-    for (let i = 0; i < controllers.length; i++) {
-        let con = controllers[i];
-        if (con != null && con.connected) {
-            indices.push(i);
-            register += `::${con.id}%%%${con.buttons.length}%%%${con.axes.length}`;
-        }
-    };
-    if (indices.length == 0) {
-        bridgeCommand('contanki::message::No controllers detected. Please reconnect your controller and try again.');
-    } else {
-        if (indices.length > 1) { 
-            bridgeCommand(register); 
-        }
-        connect_controller(indices[0]);
-    }
+   let controllers = window.navigator.getGamepads();
+   let register = "contanki::register";
+   indices = [];
+   for (let i = 0; i < controllers.length; i++) {
+      let con = controllers[i];
+      if (con != null && con.connected) {
+         indices.push(i);
+         register += `::${con.id}%%%${con.buttons.length}%%%${con.axes.length}`;
+      }
+   }
+   if (indices.length == 0) {
+      bridgeCommand(
+         "contanki::message::No controllers detected. Please reconnect your controller and try again."
+      );
+   } else {
+      if (indices.length > 1) {
+         bridgeCommand(register);
+      }
+      connect_controller(indices[0]);
+   }
 }
 
 function connect_controller(i) {
-    window.clearInterval(polling);
-    let con = window.navigator.getGamepads()[i];
-    if (con == null) {
-        bridgeCommand('contanki::message::Could not find controller. Please reconnect your controller and try again.');;
-        return;
-    }
-    bridgeCommand(`contanki::on_connect::${con.buttons.length}::${con.axes.length}::${con.id}`);
-    connected_index = i;
-    polling = setInterval(poll, 50);
+   window.clearInterval(polling);
+   let con = window.navigator.getGamepads()[i];
+   if (con == null) {
+      bridgeCommand(
+         "contanki::message::Could not find controller. Please reconnect your controller and try again."
+      );
+      return;
+   }
+   bridgeCommand(`contanki::on_connect::${con.buttons.length}::${con.axes.length}::${con.id}`);
+   connected_index = i;
+   polling = setInterval(poll, 50);
 }
 
 function on_controller_disconnect(event) {
-    bridgeCommand(`contanki::on_disconnect::arg`);
-    window.clearInterval(polling);
-    connected_index = null;
-    let controllers = window.navigator.getGamepads();
-    for (let i = 0; i < controllers.length; i++) {
-        if (controllers[i] != null) {
-            on_controller_connect(event);
-            return;
-        }
-    }
+   bridgeCommand(`contanki::on_disconnect::arg`);
+   window.clearInterval(polling);
+   connected_index = null;
+   let controllers = window.navigator.getGamepads();
+   for (let i = 0; i < controllers.length; i++) {
+      if (controllers[i] != null) {
+         on_controller_connect(event);
+         return;
+      }
+   }
 }
 
 function poll() {
-    if (connected_index == null) {
-        on_controller_disconnect();
-        return;
-    }
+   if (connected_index == null) {
+      on_controller_disconnect();
+      return;
+   }
 
-    let con = window.navigator.getGamepads()[connected_index];
+   let con = window.navigator.getGamepads()[connected_index];
 
-    try {
-        if (!con.connected) {
-            on_controller_disconnect();
-            return;
-        }
-    } catch (err) {
-        on_controller_disconnect();
-        return;
-    }
+   try {
+      if (!con.connected) {
+         on_controller_disconnect();
+         return;
+      }
+   } catch (err) {
+      on_controller_disconnect();
+      return;
+   }
 
-    let buttons = con.buttons.map(button => button.pressed);
-    bridgeCommand(`contanki::poll::${buttons}::${con.axes}`);
+   let buttons = con.buttons.map((button) => button.pressed);
+   bridgeCommand(`contanki::poll::${buttons}::${con.axes}`);
+}
+
+function mock_controller() {
+   if (connected_index != null) {
+      return;
+   }
+   bridgeCommand(`contanki::on_connect::10::4::mock_controller`);
+   polling = setInterval(mock_poll, 20);
+}
+
+function mock_poll() {
+   if ((mock_index = 100)) {
+      mock_index = 0;
+   }
+   if (mock_index < 20) {
+      bridgeCommand(
+         `contanki::poll::true,false,false,false,false,false,false,false,false,false::0,0,0,0`
+      );
+   } else {
+      bridgeCommand(
+         `contanki::poll::false,false,false,false,false,false,false,false,false,false::0,0,0,0`
+      );
+   }
+   mock_index += 1;
 }
 
 function get_controller_info() {
-    let controllers = window.navigator.getGamepads();
-    let ids = "";
-    for (let i = 0; i < controllers.length; i++) {
-        let con = controllers[i];
-        if (con != null && con.connected) {
-            ids += `%%%${con.id}%${con.buttons.length}%${con.axes.length}`;
-        }
-    }
-    return ids
+   let controllers = window.navigator.getGamepads();
+   let ids = "";
+   for (let i = 0; i < controllers.length; i++) {
+      let con = controllers[i];
+      if (con != null && con.connected) {
+         ids += `%%%${con.id}%${con.buttons.length}%${con.axes.length}`;
+      }
+   }
+   return ids;
 }
